6acb4562e4c9af2a0c85cccac1533e5b9626479d
Maicon <mayconalbert188@gmail.com>
2026-07-09 22:52:51 -0300

    09/07/2026-Cunfigurado tailwind

---
+++ src/pages/index.jsx
@@ -0,0 +1,11 @@
+
+export default function Home() {
+  return (
+    <div className={`
+    flex h-screen justify-center items-center 
+    bg-gradient-to-r from-purple-500 via-pink-800 via-pink-700
+    `}>
+      <span>Texto</span>
+    </div>
+  )
+}
