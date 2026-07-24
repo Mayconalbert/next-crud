07ed23bef273e471ddfc9a179cd0fcb4be27f3e3
Maicon <mayconalbert188@gmail.com>
2026-07-23 23:41:35 -0300

    23/07/2023 estiliza linhas e cabeçalho da tabela

--- src/components/Titulo.tsx
+++ src/components/Titulo.tsx
@@ -1,7 +1,7 @@
 export default function Titulo(props){
     return(
         <div className="flex flex-col justify-center">
-            <h1 className="px-5 py-2">
+            <h1 className="px-5 pb-2">
                 {props.children}
                 </h1>
                 <hr className="border-2 border-purple-500"/>
